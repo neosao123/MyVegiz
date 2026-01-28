1a5db993328bbd8dc92e2d09407b8d2d5e814b7f
Shruti Patil <shrutipatil1088@gmail.com>
2026-01-28 17:36:47 +0530

    implement web slider list api

--- app/api/v1/web/router.py
+++ app/api/v1/web/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
-from app.api.v1.web.routes import auth
+from app.api.v1.web.routes import auth,web_slider
 
 router = APIRouter(prefix="/web", tags=["Web"])
 
 router.include_router(auth.router, prefix="/auth")
+router.include_router(web_slider.router, prefix="/web_slider")
+
---
+++ app/api/v1/web/routes/web_slider.py
@@ -0,0 +1,53 @@
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy.orm import Session
+import math
+
+from app.api.dependencies import get_db
+from app.schemas.slider import SliderResponse
+from app.schemas.response import PaginatedAPIResponse
+from app.services.web_slider_service import list_sliders  
+
+router = APIRouter()
+
+
+@router.get("/list", response_model=PaginatedAPIResponse[list[SliderResponse]])
+def list_web_sliders(
+    page: int = Query(1, ge=1),
+    limit: int = Query(10, ge=1),
+    db: Session = Depends(get_db),
+):
+    try:
+        offset = (page - 1) * limit
+
+        total_records, sliders = list_sliders(db, offset, limit)
+
+        total_pages = math.ceil(total_records / limit) if limit else 1
+
+        pagination = {
+            "total": total_records,
+            "per_page": limit,
+            "current_page": page,
+            "total_pages": total_pages,
+        }
+
+        if sliders:
+            return {
+                "status": 200,
+                "message": "Sliders fetched successfully",
+                "data": sliders,
+                "pagination": pagination
+            }
+
+        return {
+            "status": 300,
+            "message": "No sliders found",
+            "data": [],
+            "pagination": pagination
+        }
+
+    except Exception:
+        return {
+            "status": 500,
+            "message": "Failed to fetch sliders",
+            "data": [],
+        }
